3d0b20065fdeb563796576ad7691c8029b947841
jihad-hossain1 <jihadkhan934@gmail.com>
2023-04-26 06:43:33 +0600

    card total view, rating point with react-rating react-symbol react-icon uses, author details, author name dynamically show

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -1,12 +1,34 @@
+import moment from "moment/moment";
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import {
+  FaRegBookmark,
+  FaRegEye,
+  FaRegStar,
+  FaShareAlt,
+  FaStar,
+  FaStarHalf,
+  FaStarHalfAlt,
+} from "react-icons/fa";
+import Rating from "react-rating";
 
 const NewsCard = ({ news }) => {
-  const { _id, title, image_url, details } = news;
+  const { _id, title, image_url, details, author, total_view, rating } = news;
   return (
     <Card className="mb-4">
-      <Card.Header>Featured</Card.Header>
+      <Card.Header className="d-flex align-items-center">
+        <Image style={{ height: "40px" }} src={author?.img} roundedCircle />
+        <div className="ms-2 flex-grow-1">
+          <p className="mb-0">{author?.name}</p>
+          <p>
+            <small>{moment(author?.published_date).format("yyyy-mm-DD")}</small>
+          </p>
+        </div>
+        <div>
+          <FaRegBookmark></FaRegBookmark> <FaShareAlt></FaShareAlt>
+        </div>
+      </Card.Header>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Img variant="top" src={image_url} />
@@ -21,7 +43,22 @@ const NewsCard = ({ news }) => {
           )}
         </Card.Text>
       </Card.Body>
-      <Card.Footer className="text-muted">2 days ago</Card.Footer>
+      <Card.Footer className="text-muted d-flex align-items-center">
+        <div className="flex-grow-1">
+          <Rating
+            placeholderRating={rating?.number}
+            readonly
+            emptySymbol={<FaRegStar></FaRegStar>}
+            placeholderSymbol={<FaStar className="text-warning"></FaStar>}
+            fullSymbol={<FaStar></FaStar>}
+          />
+          <span className="ms-2">{rating?.number}</span>
+        </div>
+        <div className="">
+          <FaRegEye></FaRegEye>
+          <span className="ms-2">{total_view}</span>
+        </div>
+      </Card.Footer>
     </Card>
   );
 };
