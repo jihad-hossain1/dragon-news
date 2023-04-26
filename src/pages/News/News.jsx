4c363e500130c8178c76717bc8e22726dd6b5ded
jihad-hossain1 <jihadkhan934@gmail.com>
2023-04-26 07:34:02 +0600

    EditorsIsight section added

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { Button, Card } from "react-bootstrap";
+import { FaArrowCircleLeft } from "react-icons/fa";
+import { Link, useLoaderData } from "react-router-dom";
+import EditorsInsight from "./EditorsInsight/EditorsInsight";
 
 const News = () => {
+  const news = useLoaderData();
+  const { title, image_url, details, author, total_view, rating, category_id } =
+    news;
   return (
     <div>
-      <h4>News comming soon....</h4>
+      <Card>
+        <Card.Img variant="top" src={image_url} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{details}</Card.Text>
+          <Link to={`/category/${category_id}`}>
+            <Button variant="danger">
+              <FaArrowCircleLeft className="me-2"></FaArrowCircleLeft>
+              All news in this category
+            </Button>
+          </Link>
+        </Card.Body>
+      </Card>
+      <EditorsInsight></EditorsInsight>
     </div>
   );
 };
